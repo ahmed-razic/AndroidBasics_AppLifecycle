e00f45b62731a12eee9aef3e4bed5d1c3ccbd980
ahmed-razic <ahmed.razic@gmail.com>
2021-01-29 18:19:29 +0100

    finished

--- app/src/main/java/com/example/android/androidbasics_applicationlifecycle/MainActivity.java
+++ app/src/main/java/com/example/android/androidbasics_applicationlifecycle/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("Main", "App created");
         setContentView(R.layout.activity_main);
     }
 
