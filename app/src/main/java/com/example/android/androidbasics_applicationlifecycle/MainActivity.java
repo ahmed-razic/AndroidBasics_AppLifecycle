88a0de535815cb4f406c9e5e6a0d848648eeb974
ahmed-razic <ahmed.razic@gmail.com>
2021-01-29 18:16:47 +0100

    added override methods of lifecycle

--- app/src/main/java/com/example/android/androidbasics_applicationlifecycle/MainActivity.java
+++ app/src/main/java/com/example/android/androidbasics_applicationlifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.androidbasics_applicationlifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Main", "App started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Main", "App resumed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Main", "App paused");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Main", "App stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Main", "App destroyed");
     }
 }
